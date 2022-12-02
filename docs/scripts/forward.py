ca7f2962656d1663b20f6a27a2b60b3b74751260
SuperManito <68613938+SuperManito@users.noreply.github.com>
2022-12-02 10:46:25 +0800

    Update

--- docs/scripts/forward.py
+++ docs/scripts/forward.py
@@ -1,8 +1,8 @@
 '''
 活动线报人形 PagerMaid-Pyro Bot 监控插件（一键命令版）
 Author: SuperManito
-Version: 1.9
-Modified: 2022-11-21
+Version: 2.0
+Modified: 2022-12-02
 
 友情提示：如果阁下喜欢用记事本编辑此脚本，那么如果报错了请不要在群里问，容易挨打
 
@@ -19,26 +19,31 @@ from pagermaid.single_utils import sqlite
 from pagermaid.enums import Client, Message
 from pagermaid.utils import lang, client
 from pagermaid.listener import listener
+from datetime import datetime
 
 from asyncio import sleep
-import re, os
+import re, os, time
 
-## ⚠ 定义你的容器 Bot id
+## ⚠ 容器Bot id
 ID_BOT = 1234567890
-
+## 调试模式
+DEBUG_MODE = False
 
 ## 处理命令
 async def filters(text):
+
+    def getSqlite(value):
+        return sqlite.get(f"forwardMark." + value)
+
     # 初始化一些变量
-    is_lzkj = is_lzkjdz = is_cjhy = is_cjhydz = is_txzj = False # 过滤标记
+    is_lzkj = is_lzkjdz = is_cjhy = is_cjhydz = is_txzj = enable_proxy = False # 过滤标记
     NowHour = os.popen("echo -n $(TZ=UTC-8 date +%H)").read() # 读取当前北京时间的小时数
 
     # ⚠ 用户需知:
     # 1. return False 或返回空值为不执行任何命令即不监控对应线报
-    # 2. 注意代码格式与缩进，建议使用带有语法检测的代码编辑器
-    # 3. 超级无线的活动脚本运行账号不要太多，否则ip会黑的很快，目前没有队列功能
-    # 4. 你需要了解各个脚本所对应的活动玩法以及活动域名，不要盲目设置监控，不建议监控任何开卡
-    # 5. 在 try 作用域下代码报错会自动向你的bot发送错误信息，但是不一定会影响监控的正常运行
+    # 2. 注意代码格式与缩进，建议使用带有语法检测的专业代码编辑器
+    # 3. 你需要了解各个脚本所对应的活动玩法以及活动域名，不要盲目设置监控，不建议监控任何开卡
+    # 4. 在 try 作用域下代码报错会自动向你的bot发送错误信息，但是不一定会影响监控的正常运行
 
     try:
 
@@ -47,12 +52,21 @@ async def filters(text):
             LZKJ_RUNS = " -c 1-2"
             CJHY_RUNS = " -c 1-2"
             TXZJ_RUNS = " -c 1-2"
-            ADDCARTS_RUNS = " -c 1-2"
+            ADDCARTS_RUNS = " -c 1"
         else:
             LZKJ_RUNS = " -c 1-4"
             CJHY_RUNS = " -c 1-4"
             TXZJ_RUNS = " -c 1-4"
-            ADDCARTS_RUNS = " -c 1-4"
+            ADDCARTS_RUNS = " -c 1"
+
+
+        ## 定义针对对应类型的脚本是否启用 HTTP/HTTPS 全局代理（--agent）
+        LZKJ_PROXY = False
+        LZKJDZ_PROXY = False
+        CJHY_PROXY = False
+        CJHYDZ_PROXY = False
+        TXZJ_PROXY = False
+
 
         ## 常规脚本匹配
         if "task env edit " in text:
@@ -80,117 +94,150 @@ async def filters(text):
             # cjhy 域名活动为超级会员
             # txzj 域名活动为收藏大师
 
+            match script:
 
-            # 店铺抽奖 · 超级无线/超级会员
-            if script == 'jd_luck_draw.js':
-                text = text + LZKJ_RUNS if is_lzkj else text + CJHY_RUNS
+                # 店铺抽奖 · 超级无线/超级会员
+                case 'jd_luck_draw.js':
+                    text += LZKJ_RUNS if is_lzkj else CJHY_RUNS
 
-            # 加购有礼 · 超级无线/超级会员
-            elif script == 'jd_wxCollectionActivity.js':
-                text += ADDCARTS_RUNS
+                # 加购有礼 · 超级无线/超级会员
+                case 'jd_wxCollectionActivity.js':
+                    text += ADDCARTS_RUNS
 
-            # 关注店铺有礼 · 超级无线/超级会员
-            elif script == 'jd_wxShopFollowActivity.js':
-                text = text + LZKJ_RUNS if is_lzkj else text + CJHY_RUNS
+                # 关注店铺有礼 · 超级无线/超级会员
+                case 'jd_wxShopFollowActivity.js':
+                    text += LZKJ_RUNS if is_lzkj else CJHY_RUNS
 
-            # 店铺礼包 · 超级无线/超级会员
-            elif script == 'jd_wxShopGift.js':
-                text = text + LZKJ_RUNS if is_lzkj else text + CJHY_RUNS
+                # 店铺礼包 · 超级无线/超级会员
+                case 'jd_wxShopGift.js':
+                    text += LZKJ_RUNS if is_lzkj else CJHY_RUNS
 
-            # 知识超人 · 超级无线/超级会员
-            elif script == 'jd_wxKnowledgeActivity.js':
-                text = text + LZKJ_RUNS if is_lzkj else text + CJHY_RUNS
+                # 知识超人 · 超级无线/超级会员
+                case 'jd_wxKnowledgeActivity.js':
+                    text += LZKJ_RUNS if is_lzkj else CJHY_RUNS
 
-            # 店铺抽奖中心 · 超级无线
-            elif script == 'jd_drawCenter.js':
-                text += LZKJ_RUNS
-                is_lzkj = True # 用于屏蔽标记判断（勿动）
+                # 店铺抽奖中心 · 超级无线
+                case 'jd_drawCenter.js':
+                    text += LZKJ_RUNS
+                    is_lzkj = True # 用于屏蔽标记判断（勿动）
 
-            # 读秒拼手速 · 超级无线
-            elif script == 'jd_wxSecond.js':
-                text += LZKJ_RUNS
-                is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
+                # 读秒拼手速 · 超级无线
+                case 'jd_wxSecond.js':
+                    text += LZKJ_RUNS
+                    is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
 
-            # 无线游戏 · 超级无线
-            elif script == 'jd_wxgame.js':
-                text += LZKJ_RUNS
-                is_lzkj = True # 用于屏蔽标记判断（勿动）
+                # 无线游戏 · 超级无线
+                case 'jd_wxgame.js':
+                    text += LZKJ_RUNS
+                    is_lzkj = True # 用于屏蔽标记判断（勿动）
 
-            # 集卡有礼 · 超级无线
-            elif script == 'jd_wxCollectCard.js':
-                text += LZKJ_RUNS
-                is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
+                # 集卡有礼 · 超级无线
+                case 'jd_wxCollectCard.js':
+                    text += LZKJ_RUNS
+                    is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
 
-            # 粉丝互动 · 超级无线
-            elif script == 'jd_wxFansInterActionActivity.js':
-                text += LZKJ_RUNS
-                is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
+                # 粉丝互动 · 超级无线
+                case 'jd_wxFansInterActionActivity.js':
+                    text += LZKJ_RUNS
+                    is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
 
-            # 分享有礼 · 超级无线
-            elif script == 'jd_wxShareActivity.js':
-                text = text
-                is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
+                # 分享有礼 · 超级无线
+                case 'jd_wxShareActivity.js':
+                    text = text
+                    is_lzkj = is_lzkjdz = True # 用于屏蔽标记判断（勿动）
 
-            # 组队瓜分奖品 · 超级无线
-            elif script == 'jd_zdjr.js':
-                text = text
+                # 组队瓜分奖品 · 超级无线
+                case 'jd_zdjr.js':
+                    text = text
 
-            # 组队瓜分奖品 · 超级会员
-            elif script == 'jd_cjzdgf.js':
-                text = text
+                # 组队瓜分奖品 · 超级会员
+                case 'jd_cjzdgf.js':
+                    text = text
 
-            # 加购有礼 · 收藏大师
-            elif script == 'jd_txzj_cart_item.js':
-                text += ADDCARTS_RUNS
+                # 加购有礼 · 收藏大师
+                case 'jd_txzj_cart_item.js':
+                    text += ADDCARTS_RUNS
 
-            # 关注商品有礼 · 收藏大师
-            elif script == 'jd_txzj_collect_item.js':
-                text += TXZJ_RUNS
+                # 关注商品有礼 · 收藏大师
+                case 'jd_txzj_collect_item.js':
+                    text += TXZJ_RUNS
 
-            # 关注店铺有礼 · 收藏大师
-            elif script == 'jd_txzj_collect_shop.js':
-                text += TXZJ_RUNS
+                # 关注店铺有礼 · 收藏大师
+                case 'jd_txzj_collect_shop.js':
+                    text += TXZJ_RUNS
 
-            # 幸运抽奖 · 收藏大师
-            elif script == 'jd_txzj_lottery.js':
-                text += TXZJ_RUNS
+                # 幸运抽奖 · 收藏大师
+                case 'jd_txzj_lottery.js':
+                    text += TXZJ_RUNS
 
-            # 购物车锦鲤 · 超级无线
-            # elif script == 'jd_wxCartKoi.js':
-            #     text += ADDCARTS_RUNS
-            #     is_lzkj = True
+                # 购物车锦鲤 · 超级无线
+                # case 'jd_wxCartKoi.js':
+                #     text += ADDCARTS_RUNS
+                #     is_lzkj = True
 
-            # 微定制瓜分京豆 · 超级会员
-            # elif script == 'jd_wdz.js':
-            #     text = text
-            #     is_cjhy = True
+                # 微定制瓜分京豆 · 超级会员
+                # case 'jd_wdz.js':
+                #     text = text
+                #     is_cjhy = True
 
-            # 大牌集合 · 京耕
-            # elif script == 'jd_opencardDPLHTY.js':
-            #     text = text
+                # 大牌集合 · 京耕
+                # case 'jd_opencardDPLHTY.js':
+                #     text = text
 
-            # joyjd抽奖机
-            # elif script == 'jd_lottery.js':
-            #     text = text
+                # joyjd抽奖机
+                # case 'jd_lottery.js':
+                #     text = text
 
-            # joyjd开卡
-            # elif script == 'jd_joyopen.js':
-            #     text = text
+                # joyjd开卡
+                # case 'jd_joyopen.js':
+                #     text = text
+
+                case _:
+                    await debugMode("未匹配到对应监控脚本")
+                    return False
 
         else:
             return False
 
-        ## 监控屏蔽（勿动）
-        if (is_lzkj) and (sqlite.get("forwardMark.disable_lzkj")):
-            return False
-        if (is_lzkjdz) and (sqlite.get("forwardMark.disable_lzkjdz")):
-            return False
-        if (is_cjhy) and (sqlite.get("forwardMark.disable_cjhy")):
-            return False
-        if (is_cjhydz) and (sqlite.get("forwardMark.disable_cjhydz")):
-            return False
-        if (is_txzj) and (sqlite.get("forwardMark.disable_txzj")):
-            return False
+        ## 监控屏蔽和脚本代理（勿动）
+        if is_lzkj:
+            if getSqlite("disable_lzkj"):
+                await debugMode("超级无线活动已被屏蔽")
+                return False
+            if LZKJ_PROXY:
+                enable_proxy = True
+        elif is_lzkjdz:
+            if getSqlite("disable_lzkjdz"):
+                await debugMode("超级无线（定制）活动已被屏蔽")
+                return False
+            if LZKJDZ_PROXY:
+                enable_proxy = True
+        elif is_cjhy:
+            if getSqlite("disable_cjhy"):
+                await debugMode("超级会员活动已被屏蔽")
+                return False
+            if CJHY_PROXY:
+                enable_proxy = True
+        elif is_cjhydz:
+            if getSqlite("disable_cjhydz"):
+                await debugMode("超级会员（定制）活动已被屏蔽")
+                return False
+            if CJHYDZ_PROXY:
+                enable_proxy = True
+        elif is_txzj:
+            if getSqlite("disable_txzj"):
+                await debugMode("收藏大师活动已被屏蔽")
+                return False
+            if TXZJ_PROXY:
+                enable_proxy = True
+
+        ## 设置一些脚本运行参数
+        if " now" in text:
+            # 迅速模式
+            text += " -r"
+            # 脚本 HTTP/HTTPS 全局代理
+            if enable_proxy:
+                text += " -a"
 
         text = "/cmd " + text
         return text
@@ -225,7 +272,7 @@ async def forward(message: Message):
     errMsg = "出错了呜呜呜 ~ 无法识别的来源对话。"
 
     if ID_BOT == '1234567890':
-        await message.edit("⚠ 请先在此脚本中定义你的 BOT id 后才能使用哦~")
+        await message.edit("⚠ 请先在此脚本中定义你的容器 BOT id 后才能使用哦~")
         await sleep(5)
         await message.delete()
         return
@@ -346,12 +393,14 @@ async def forward_message(message: Message):
         if text != '' and '`' in text:
             text = text.split('`')[1]
         else:
+            await debugMode("线报内容的语法格式不符合要求")
             return
 
         ## 去解析命令
         results = await filters(text)
-        await log(f"forward 监控到新消息：{str(text)}")
+        await log(f"forward 监控到新消息：{str(text)}") # 打印日志
         if not results:
+            await debugMode("线报经过函数处理后返回为空")
             return
 
         if results != '':
@@ -362,5 +411,11 @@ async def forward_message(message: Message):
         await log(errorMsg)
         await bot.send_message(int(ID_BOT), "❌ 脚本报错了！\n\n错误内容：" + errorMsg)
         return False
+
+
+async def debugMode(msg):
+    if DEBUG_MODE:
+        timeStr = str(datetime.fromtimestamp(int(time.time())))
+        await bot.send_message(int(ID_BOT), timeStr + f"\n🔧 debug: {msg}")
 
 ## ⬆️ 不懂勿动 ⬆️
